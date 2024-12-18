5acbf6664e9dd284be0e18928ea0ca17ab1e6c3d
valid <janipatrik138@gmail.com>
2024-12-18 09:17:40 +0100

    removed missing image ref

--- src/pages/adminPages/AdminPage.vue
+++ src/pages/adminPages/AdminPage.vue
@@ -3,7 +3,7 @@
 
 import AdminLayout from "../../slots/AdminLayout.vue";
 import AdminNavbar from "../../components/adminComponents/AdminNavbar.vue";
-import {useRoute, useRouter} from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const router = useRouter()
 
@@ -22,15 +22,15 @@ const goBack = () => {
       <section class=" w-full h-full border-r-[1px] border-r-white">
         <div class="w-full h-[7dvh] flex justify-between px-4 flex-nowrap gap-[7rem]">
           <button class="text-4xl" @click="goBack">←</button>
-          <img src="/src/public/images/person-circle.svg" alt="fekete" class="size-12 my-3 invert">
         </div>
-        <AdminNavbar/>
+        <AdminNavbar />
       </section>
     </template>
     <template #right>
       <section class="w-full h-full">
         <section v-if="route.path === '/admin'" class="h-full">
-          <div class="flex w-full justify-center items-center text-2xl h-[7dvh] font-bold border-b-[1px] border-b-white">
+          <div
+            class="flex w-full justify-center items-center text-2xl h-[7dvh] font-bold border-b-[1px] border-b-white">
             <span>Admin felület</span>
           </div>
           <div class="h-full w-full pb-56 flex justify-center items-center">
@@ -38,7 +38,7 @@ const goBack = () => {
           </div>
         </section>
         <section v-else class="w-full h-full">
-          <router-view/>
+          <router-view />
         </section>
       </section>
     </template>
