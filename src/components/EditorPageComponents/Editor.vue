cad03726c57dfa403dfd5cfa1a08bcd10460ac53
dominik1312 <129722816+dominik1312@users.noreply.github.com>
2024-12-20 21:00:33 +0100

    Update Editor.vue

---
+++ src/components/EditorPageComponents/Editor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="editor-wrapper">
+    <MdEditor
+      v-model="contents"
+      language="en-US"
+      v-on:upload-img="imgupload"
+      class="custom-editor"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MdEditor } from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+import { ref } from "vue";
+
+const contents = ref("# Hello World");
+const imgupload = (e) => {
+  console.debug("Uploaded img" + e);
+};
+</script>
+
+<style scoped>
+.custom-editor {
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  backdrop-filter: blur(10px); 
+  border-radius: 15px;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  flex-direction: column;
+}
+</style>
+
