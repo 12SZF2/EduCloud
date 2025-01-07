fd2d7a0480508b5190b8dcced343f63edc27f171
iwillsettyou <bmate20050911@gmail.com>
2025-01-07 08:21:16 +0100

    noone asked for this feature, but I made it. Now the navbar links change color based on the active component open

--- src/components/adminComponents/AdminNavbar.vue
+++ src/components/adminComponents/AdminNavbar.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import {inject, ref} from "vue";
+import { ref } from 'vue';
 
 const showModulesDropdown = ref(false);
 const showAssignmentsDropdown = ref(false);
 const showUsersDropdown = ref(false);
+const clickedLink = ref<string | null>(null);
 
-const model = defineModel()
-
-console.log(model.value)
+const handleLinkClick = (link: string) => {
+  clickedLink.value = link;
+};
 </script>
 
 <template>
-
   <section>
-    <div class="flex flex-col border-t-[1px] border-t-[--border-color] text-[--text-color] sm:text-xl md:text-1xl
-                ">
+    <div class="flex flex-col border-t-[1px] border-t-[--border-color] text-[--text-color] sm:text-xl md:text-1xl">
       <!-- Modules Dropdown -->
       <div class="ml-1">
         <div class="py-2 mt-2">
@@ -24,29 +23,29 @@ console.log(model.value)
           >
             Modulok
           </button>
-          <div
-              v-if="showModulesDropdown"
-              class=""
-          >
+          <div v-if="showModulesDropdown">
             <ul class="list-disc list-inside">
-              <!--add another RouterLink to add new path-->
               <RouterLink
                   to="/admin/module-create"
                   class="block px-4 text-lg"
-
+                  :style="{ color: clickedLink === 'module-create' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('module-create')"
               >
                 <li>Létrehozás</li>
               </RouterLink>
               <RouterLink
                   to="/admin/module-list"
                   class="block px-4 text-lg"
-
+                  :style="{ color: clickedLink === 'module-list' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('module-list')"
               >
                 <li>Listázás</li>
               </RouterLink>
               <RouterLink
                   to="/admin/module-category-create"
                   class="block px-4 text-lg"
+                  :style="{ color: clickedLink === 'module-category-create' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('module-category-create')"
               >
                 <li>Kategória létrehozása</li>
               </RouterLink>
@@ -62,49 +61,51 @@ console.log(model.value)
           >
             Feladatok
           </button>
-          <div
-              v-if="showAssignmentsDropdown"
-              class=""
-          >
+          <div v-if="showAssignmentsDropdown">
             <ul class="list-disc list-inside">
-            <RouterLink
-                to="/admin/assignment-create"
-                class="block px-4 text-lg"
-            >
-              <li>Létrehozás</li>
-            </RouterLink>
-            <RouterLink
-                to="/admin/assignment-list"
-                class="block px-4 text-lg"
-            >
-              <li>Listázás</li>
-            </RouterLink>
+              <RouterLink
+                  to="/admin/assignment-create"
+                  class="block px-4 text-lg"
+                  :style="{ color: clickedLink === 'assignment-create' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('assignment-create')"
+              >
+                <li>Létrehozás</li>
+              </RouterLink>
+              <RouterLink
+                  to="/admin/assignment-list"
+                  class="block px-4 text-lg"
+                  :style="{ color: clickedLink === 'assignment-list' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('assignment-list')"
+              >
+                <li>Listázás</li>
+              </RouterLink>
             </ul>
           </div>
         </div>
 
         <!-- Users Dropdown -->
-        <div class="py-2" >
+        <div class="py-2">
           <button
               @click="showUsersDropdown = !showUsersDropdown"
               class="w-full px-4 text-left underline flex items-center"
           >
             Felhasználók
           </button>
-          <div
-              v-if="showUsersDropdown"
-              class=""
-          >
+          <div v-if="showUsersDropdown">
             <ul class="list-disc list-inside">
               <RouterLink
                   to="/admin/user-create"
                   class="block px-4 text-lg"
+                  :style="{ color: clickedLink === 'user-create' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('user-create')"
               >
                 <li>Létrehozás</li>
               </RouterLink>
               <RouterLink
                   to="/admin/user-list"
                   class="block px-4 text-lg"
+                  :style="{ color: clickedLink === 'user-list' ? 'var(--active-page-link-color)' : '' }"
+                  @click="handleLinkClick('user-list')"
               >
                 <li>Listázás</li>
               </RouterLink>
@@ -114,5 +115,4 @@ console.log(model.value)
       </div>
     </div>
   </section>
-
 </template>
